24328bdf0fdb03be9bb47b333da39616e7a5daac
Ruby Felton <ruby.felton@oit.edu>
2018-08-06 17:53:45 -0700

    add remove functionality to drawing and composite

--- CompositeFactory/CompositeFactory/Drawing.cpp
+++ CompositeFactory/CompositeFactory/Drawing.cpp
@@ -6,6 +6,11 @@ void Drawing::Add(ShapeComponent& component)
 	shapes.push_back(component);
 }
 
+void Drawing::Remove(ShapeComponent& component)
+{
+	shapes.erase(std::remove(shapes.begin(), shapes.end(), component), shapes.end());
+}
+
 Drawing::iterator Drawing::seeBegin()
 {
 	return shapes.begin();
--- CompositeFactory/CompositeFactory/PictureComposite.cpp
+++ CompositeFactory/CompositeFactory/PictureComposite.cpp
@@ -3,10 +3,12 @@
 
 void PictureComposite::AddComponent(ShapeComponent& component)
 {
+	composite_shapes.push_back(component);
 }
 
 void PictureComposite::remove_component(ShapeComponent& component)
 {
+	composite_shapes.erase(std::remove(composite_shapes.begin(), composite_shapes.end(), component), composite_shapes.end());
 }
 
 ShapeComponent& PictureComposite::get_child(int index)
